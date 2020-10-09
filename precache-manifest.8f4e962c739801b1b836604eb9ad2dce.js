bb87c7a114bb7cee847f9c86fe5dd892b3feb7ae
Saqib Naveed <saqibnaveed27@gmail.com>
2020-10-10 00:08:33 +0500

    Updates

---
+++ precache-manifest.8f4e962c739801b1b836604eb9ad2dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "184a0161c86bcd8661bf979c4d93c672",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bba66d7769e81a21b7f4",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/static/css/main.32fbb8fa.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bba66d7769e81a21b7f4",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/static/js/main.c8a5d14d.chunk.js"
+  },
+  {
+    "revision": "4db6d77ba3961749c454",
+    "url": "/://saqibnaveed.github.io/monsters-rolodex/static/js/runtime-main.34d73abe.js"
+  }
+]);
